92fb5ac3ffbdf3c718bfe16dd5d77fc31498ceb6
Nite2005 <kumarnitesh140905@gmail.com>
2025-09-28 19:48:11 +0530

    error in model evaluation in dagshub authentication

--- src/models/model_evaluation.py
+++ src/models/model_evaluation.py
@@ -14,8 +14,8 @@ dagshub_token = os.getenv("DAGSHUB_TOKEN")
 if not dagshub_token:
     raise EnvironmentError("DAGSHUB_TOKEN environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 # dagshub.init(repo_owner='Nite2005', repo_name='delivery-time-prediction', mlflow=True)
 
 # set the tracking server
